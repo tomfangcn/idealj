49ff9690bbd53e6e8414256c1a1f203bb33d0f25
tomfangcn <910376698@qq.com>
2022-01-15 09:09:14 +0800

    测试ddr3 内存上线

--- demo01/src/main/java/Demo.java
+++ demo01/src/main/java/Demo.java
@@ -7,5 +7,18 @@ public class Demo {
         System.out.println("world");
         System.out.println("测试仓库变更");
         System.out.println("本地远程变更再测");
+
+
+        /*A[] a = new A[1000];
+        for (int i = 0; i < 1000; i++) {
+            a[i] = new A();
+        }*/
+    }
+    
+    public static  class A {
+        private byte[] a ;
+        public A(){
+            a = new byte[256*1024*1024];
+        }
     }
 }
